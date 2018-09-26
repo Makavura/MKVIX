7834ee8547e4ee7d47a2fd947f8b1e9542e5ceb8
Makavura <makavuramughanga@gmail.com>
2018-09-27 00:22:09 +0300

    restructured the whole app testing endpoints with curl

--- test_app.py
+++ test_app.py
@@ -1,35 +1,34 @@
-import os
+from flask import Flask
 import unittest
+import json
 from app.__init__ import create_app
 
+app = Flask(__name__)
 
-class TestAll(unittest.TestCase):
 
-  def setUp(self):
-    self.app = create_app("test")
-    self.client = self.app.test_client()
-    self.app_context = self.app.app_context()
-    self.app_context.push()
-    self.new_order = {"meal": "Sanchezium",
-                      "address": "Hunters Pub", "done": True}
+class Testendpoints(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app("test")
+        self.client = self.app.test_client()
+        self.app_context = self.app.app_context()
+        self.app_context.push()
 
-  def test_get(self):
-    respond = self.client.get('/api/v1/orders',
-                              headers={"content-type": "application/json"})
+    def test_get(self):
+        respond = self.client.get('/orders/api/v1/orders',
+                                  headers={"content-type": "application/json"})
 
-  def test_get(self):
-    respond = self.client.get('/api/v1/orders/<int:order_id',
-                              headers={"content-type": "application/json"})
+    def test_get(self):
+        respond = self.client.get('/orders/api/v1/orders/<int:order_id',
+                                  headers={"content-type": "application/json"})
 
-  def test_post(self):
-    respond = self.client.post('/api/v1/orders',
-                               headers={"content-type": "application/json"})
+    def test_post(self):
+        respond = self.client.post('/orders/api/v1/orders',
+                                   headers={"content-type": "application/json"})
 
-  def test_put(self):
-    respond = self.client.put('/api/v1/orders/<int:order_id>',
-                              headers={"content-type": "application/json"})
+    def test_put(self):
+        respond = self.client.put('/orders/api/v1/orders/<int:order_id>',
+                                  headers={"content-type": "application/json"})
 
-  def test_delete(self):
-    respond = self.client.delete('/api/v1/orders/<int:order_id>',
-                                 headers={"content-type": "application/json"})
-
+    def test_delete(self):
+        respond = self.client.delete('/orders/api/v1/orders/<int:order_id>',
+                                     headers={"content-type": "application/json"})
