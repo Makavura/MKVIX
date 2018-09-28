35be3763f2d4736aaaf33b1c70b2f7e341115bd4
Makavura <39646663+Makavura@users.noreply.github.com>
2018-09-28 17:22:46 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -6,7 +6,6 @@ from api.v1.app import app
 class Testendpoints(unittest.TestCase):
   def setUp(self):
     self.client = app.test_client()
-    
 
   def test_get_orders(self):
     request = self.client.get('/orders/api/v1/orders/',
@@ -19,7 +18,12 @@ class Testendpoints(unittest.TestCase):
     self.assertEqual(request.status_code, 200)
 
   def test_create_order(self):
-    data = {"meal": "codebalaa"}
+    data = {"meal": "codebalaa",
+            "price": 750,
+            "description": "lit",
+            "address": "here",
+            "delivered": False
+            }
     request = self.client.post("/orders/api/v1/orders/",
                                data=json.dumps(data), headers={"content-type": "application/json"})
     print(request.data)
@@ -35,5 +39,5 @@ class Testendpoints(unittest.TestCase):
   def test_delete_order(self):
     request = self.client.delete('/orders/api/v1/orders/2/',
                               headers={"content-type": "application/json"})
+
     self.assertTrue(request.status_code, 200)
-  
